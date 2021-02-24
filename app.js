b0d5c8f3108ae5258c4299176338d100358b6700
Yors <jatbribiesca@gmail.com>
2021-02-24 16:14:55 -0600

    fix typo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res) => {
+app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*")
 	res.setHeader(
 		"Access-Control-Allow-Headers",
