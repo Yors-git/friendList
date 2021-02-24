7737043a0dc8e24b9ea2d36d6d46c1320d3e2684
Yors <jatbribiesca@gmail.com>
2021-02-24 16:03:49 -0600

    try different headers

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+app.use((req, res) => {
 	res.setHeader("Access-Control-Allow-Origin", "*")
 	res.setHeader(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 	)
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
 	next()
-});
+})
 
 app.use("/", friendRoutes);
 
